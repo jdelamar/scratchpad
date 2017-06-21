d5d79c3891792b2c09400476c53edf7722a874a3
Joel De Lamarre <jdelamar@ciena.com>
2017-06-21 11:27:00 -0400

    initial commit

---
+++ scratchpad.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var waiter sync.WaitGroup
+
+func main() {
+	RunStuff()
+}
+
+func RunStuff() {
+	fmt.Printf("Oh well, that started\n")
+
+	waiter.Add(1)
+
+	go doSomething()
+	time.Sleep(10 * time.Second)
+
+	fmt.Printf("Ah! it waited 10 seconds\n")
+
+	waiter.Wait()
+
+}
+
+func doSomething() {
+
+	defer waiter.Done()
+
+	tick := 0
+
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		tick++
+		fmt.Printf("Ticked at %s\n", t.String())
+
+		if tick > 5 {
+			break
+		}
+	}
+}
---
+++ scratchpad_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestDoSomething(t *testing.T) {
+	RunStuff()
+}
